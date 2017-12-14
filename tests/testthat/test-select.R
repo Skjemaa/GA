5ca9429a98daf1de66c5ba727f6ec371d81fa035
yachuan liu <liuyachuan7119@berkeley.edu>
2017-12-14 08:29:44 -0800

    test-select, needs more input tests
    1.reg_method must be "lm" or "glm"
    
    2.n_iter,pop_size,nb_groups, must be nb_pts positive integers
    3.objective must be "AIC" or  "BIC"
    4.parent_selection must be "prop","prop_random" , "random" or "tournament".
    5. mu must be 0 to 1.

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -1,5 +1,47 @@
-context("select")
+library(testthat)
+############################################################
+######### integration test for select() function ###########
+context("Integration test for select() function")
 
-test_that("dummy test", {
-  expect_equal(select(), 1)
+
+test_that("Input errors", {
+  expect_error(select(dataset=Boston),
+               'argument "y" is missing, with no default')
+
+  expect_error(select(y="crim"),
+               'argument "dataset" is missing, with no default')  
+  
+  expect_error(select(y="crim", dataset= as.matrix(Boston)),
+               "The dataset is not a data frame.")
+  
+
 })
+
+
+test_that("Check Output object is a list", {
+  result1 <- select(y="crim", dataset=Boston, reg_method = NULL, n_iter = 200, pop_size = 2 * n, objective = "AIC",
+                    interaction = F, most_sig = F, parent_selection = "prop", nb_groups = 4, generation_gap = 0.25,
+                    gene_selection = NULL, nb_pts = 1, mu = 0.3, err = 1e-6)
+  expect_equal(class(result1), "list")
+  expect_equal(length(result1), 4)
+
+})
+
+test_that("Check that in some case, maximum number of iterations is useful",{
+  result1 <-select(y="crim", dataset=Boston, reg_method = NULL, n_iter = 20, pop_size = 2 * n, objective = "AIC",
+                  interaction = F, most_sig = F, parent_selection = "prop", nb_groups = 4, generation_gap = 0.25,
+                  gene_selection = NULL, nb_pts = 1, mu = 0.3, err = 1e-6)
+  expect_true(result1$count ==20)
+})
+
+test_that("With different mechanism, out algorithm converges to different result",{
+  result11 <- select(y="crim", dataset=Boston, parent_selection = "prop")
+  result12 <- select(y="crim", dataset=Boston, parent_selection = "random")
+  result13 <- select(y="crim", dataset=Boston, parent_selection = "prop_random")
+  result14 <- select(y="crim", dataset=Boston, parent_selection = "tournament")
+  
+  expect_false(result12$ == result11$)
+  print(result11$model)
+  print(result12$model)
+})
+ 
