eed35838f8b5bbaf5681afcd833abe945e2bbc3e
YUWANG111 <31394476+YUWANG111@users.noreply.github.com>
2017-12-14 14:52:52 -0800

    delete fitness

--- R/get_fittest_individuals.R
+++ R/get_fittest_individuals.R
@@ -5,17 +5,10 @@
 #' @param objective name of the objective function to be maximized (as a string, inside quotes)
 #' @param fitness only works with AIC, using a fitness function derived from the AIC
 #' @return returns the element of individuals with that maximizes the objective function
-get_fittest_ind <- function(individuals, objective = "AIC", fitness = F){
-  if(fitness){
-    ## case where the objective function is not the fitness function
-    # @TODO
-    return(0)
-  } 
-  else{
-    idx <- which.max(unlist(lapply(individuals, 
+get_fittest_ind <- function(individuals, objective = "AIC"){
+  idx <- which.max(unlist(lapply(individuals, 
                                    function(x) - eval(as.name(objective))(x$linear_model))))
-    return(individuals[[idx]])
-  }
+  return(individuals[[idx]])
 }
 
 #' Used to get the k elements from a list of (variables, indices, linear_model)
@@ -27,16 +20,9 @@ get_fittest_ind <- function(individuals, objective = "AIC", fitness = F){
 #' @param k number of variables we want to choose.
 #' @return returns the k elements of individuals with that have the highest objectives
 
-get_k_fittest_ind <- function(individuals, objective = "AIC", fitness = F, k){
-  if(fitness){
-    ## case where the objective function is not the fitness function
-    # @TODO
-    return(0)
-  } 
-  else{
-    idx <- order(unlist(lapply(individuals, 
+get_k_fittest_ind <- function(individuals, objective = "AIC", k){
+  idx <- order(unlist(lapply(individuals, 
                                function(x) - eval(as.name(objective))(x$linear_model))),
                  decreasing = T)
-    return(individuals[idx[1:k]])
-  }
+  return(individuals[idx[1:k]])
 }
