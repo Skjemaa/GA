61deb2f19274463b6d39ee8be5a28bbb9654dc4a
SlicedBadge <riversj@berkeley.edu>
2017-12-14 14:03:00 -0800

    fixed tests

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -12,14 +12,14 @@ test_that("Input errors", {
                'argument "dataset" is missing, with no default')
 
   expect_error(select(y="crim", dataset= as.matrix(Boston)),
-               "The dataset is not a data frame.")
+               "Y can't be found in dataset")
 
 
 })
 
 
 test_that("Check Output object is a list", {
-  result1 <- select(y="crim", dataset=Boston, reg_method = NULL, n_iter = 200, pop_size = 2 * n, objective = "AIC",
+  result1 <- select(y="crim", dataset=Boston, reg_method = NULL, n_iter = 200, objective = "AIC",
                     interaction = F, most_sig = F, parent_selection = "prop", nb_groups = 4, generation_gap = 0.25,
                     gene_selection = NULL, nb_pts = 1, mu = 0.3, err = 1e-6)
   expect_equal(class(result1), "list")
