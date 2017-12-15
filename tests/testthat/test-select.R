b5e99c0d2a8d4311166db38d79c794036f7d6bf8
YUWANG111 <31394476+YUWANG111@users.noreply.github.com>
2017-12-14 16:43:17 -0800

    Update test-select.R

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -43,3 +43,21 @@ test_that("With different mechanism, our algorithm can converge to same result",
   expect_true(result12$objective == result11$objective)
 })
 
+test_that("The variables are correctly chosen", {
+  simulation <- function(c,n,k=3){
+      X <- matrix(rep(0,n*k),n,k)
+      for (i in 1:k){
+         X[,i]<- rnorm(n, runif(1,-50,50), runif(1,0,1))
+     }
+     X <- as.data.frame(X)
+     X[,((k+1):c)] <- rnorm(n*(c-k), 0, 0.2)   
+     X$Y <- 5*X[,1]+3*X[,2]- 2*X[,3]
+     X
+     colnames(X)<- c(paste0("X", 1:c),"Y")
+     return(X)
+ }
+  X <- simulation(10,100,3)  
+  result <- select("Y", X, most_sig = T, n_iter = 500)
+  
+  expect_equal(result[[1]]$indices, c(1,2,3))
+})
